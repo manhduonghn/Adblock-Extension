8bd3e1daeacebe77db77e6e8a23d051cb0d26e62
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2024-11-25 11:17:13 +0700

    Update background.js

--- background.js
+++ background.js
@@ -1,41 +1,23 @@
-const filePath = chrome.runtime.getURL("custom_rules/block_list.txt");
-
-fetch(filePath)
+fetch(
+  "https://raw.githubusercontent.com/luxysiv/Adblock-extension/main/custom_rules/block_list.txt"
+)
   .then((response) => response.text())
-  .then((blockListText) =>
-    getExistingRulesFromStorage().then((existingRules) => {
-      // Parse block list
-      const urlFilters = blockListText
-        .split("\n")
-        .filter((line) => line && !line.startsWith("!") && !line.startsWith("["))
-        .map((line) => line.trim());
+  .then((text) => {
+    const urlFilters = text
+      .split("\n")
+      .filter((line) => line && !line.startsWith("!") && !line.startsWith("["))
+      .map((line) => line.trim());
 
-      // Extract existing rules and create a Set for deduplication
-      const existingFilters = new Set(
-        existingRules.map((rule) => rule.condition.urlFilter)
-      );
+    const rules = urlFilters.map((filter, index) => ({
+      id: index + 1,
+      priority: 1,
+      action: { type: "block" },
+      condition: { urlFilter: filter },
+    }));
 
-      // Map new URL filters to rules, skipping duplicates
-      const newRules = urlFilters
-        .filter((filter) => !existingFilters.has(filter)) // Add only non-existent filters 
-        .map((filter, index) => ({
-          id: existingRules.length + index + 1,
-          priority: 1,
-          action: { type: "block" },
-          condition: { urlFilter: filter },
-        }));
-
-      // Combine new rules with existing ones
-      const updatedRules = [...existingRules, ...newRules];
-
-      // Update dynamic rules in Chrome
-      chrome.declarativeNetRequest.updateDynamicRules({
-        addRules: newRules,
-        removeRuleIds: [], // Do not delete existing rules 
-      });
-
-      // Save updated rules to chrome.storage
-      saveRulesToStorage(updatedRules);
-    })
-  )
-  .catch((error) => console.error("Error processing block list:", error));
+    chrome.declarativeNetRequest.updateDynamicRules({
+      addRules: rules,
+      removeRuleIds: rules.map((rule) => rule.id),
+    });
+  })
+  .catch((error) => console.error("Error fetching adblock list:", error));
