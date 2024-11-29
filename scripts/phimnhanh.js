03234211c2e0fe59d09b2c26b7615cd93f3c53fa
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2024-11-29 08:18:36 +0700

    Move redirect link
    Not work

--- scripts/phimnhanh.js
+++ scripts/phimnhanh.js
@@ -13,15 +13,3 @@ style.textContent = css;
 
 // Inject the style into the HTML head
 document.documentElement.appendChild(style);
-
-// Function to redirect if the URL contains "embed.html?link="
-function redirectVideoLink() {
-    const match = window.location.href.match(/embed\.html\?link=([^&]*)/);
-    if (match) {
-        const videoLink = decodeURIComponent(match[1]);
-        window.location.href = videoLink; // Redirect to the decoded video link
-    }
-}
-
-// Call the redirect function if necessary
-redirectVideoLink();
